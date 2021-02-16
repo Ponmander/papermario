7713806134aa884b3e8d45ebc0ff7d19f9af95fe
PonderMan <ponmander@gmail.com>
2021-02-16 07:18:56 -0600

    Just trying to merge my branch with master

--- src/code_3D5020.c
+++ src/code_3D5020.c
@@ -7,7 +7,12 @@ void func_E00D6718(void){
 
 INCLUDE_ASM(s32, "code_3D5020", func_E00D6720);
 
-INCLUDE_ASM(s32, "code_3D5020", func_E00D693C);
+//INCLUDE_ASM(s32, "code_3D5020", func_E00D693C);
+
+void func_E00D693C(s32 arg0) {
+    func_E0200480(&subroutine_arg4, 0x2D, 0xA, arg0, &func_E00D6980);
+}
+
 
 void func_E00D6978(void){
 }
